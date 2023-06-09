7354b7e94e00140f56cda861bde0c9800a9b75de
Sakib963 <abdullah15-963@diu.edu.bd>
2023-06-09 21:17:37 +0600

    Added Dummy Data of Classes & Instructors

--- index.js
+++ index.js
@@ -1,19 +1,114 @@
-const express = require('express');
-const cors = require('cors');
+const express = require("express");
+const cors = require("cors");
+require("dotenv").config();
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const jwt = require("jsonwebtoken");
 
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(express.json())
+app.use(express.json());
 app.use(cors());
 
+// Middleware for JWT Verification
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res
+      .status(401)
+      .send({ error: true, message: "unauthorized access" });
+  }
+  // bearer token
+  const token = authorization.split(" ")[1];
 
-app.get('/', (req, res) => {
-    res.send('Elegant Fashion Summer School is running.')
-})
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res
+        .status(401)
+        .send({ error: true, message: "unauthorized access" });
+    }
+    req.decoded = decoded;
+    next();
+  });
+};
 
+app.get("/", (req, res) => {
+  res.send("Elegant Fashion Summer School is running.");
+});
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.poiwoh3.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+
+    const usersCollection = client.db("elegantEdgeDB").collection("users");
+    const instructorsCollection = client.db('elegantEdgeDB').collection('instructors');
+    const classesCollection = client.db('elegantEdgeDB').collection('classes');
+
+    //  JWT
+    app.post("/jwt", (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+        expiresIn: "1h",
+      });
+
+      res.send({ token });
+    });
+
+    // Create User
+    app.post("/users", async (req, res) => {
+      const userInfo = req.body;
+      const query = { email: userInfo.email };
+      const existingUser = await usersCollection.findOne(query);
+
+      if (existingUser) {
+        return res.send({ message: "User Already Exists" });
+      }
+      const result = await usersCollection.insertOne(userInfo);
+      res.send(result);
+    });
+
+    app.get('/user', async(req, res) => {
+      const result = await usersCollection.find().toArray();
+      res.send(result);
+    })
+
+    // Instructors
+    app.get('/instructors', async(req, res) => {
+      const result = await instructorsCollection.find().toArray();
+
+      res.send(result);
+    })
+
+    // Classes
+    app.get('/classes', async(req, res) => {
+      const result = await classesCollection.find().toArray();
+      res.send(result);
+    })
+
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
 
 app.listen(port, () => {
-    console.log(`Elegant Fashion Summer School is running on port: ${port}`);
+  console.log(`Elegant Fashion Summer School is running on port: ${port}`);
 });
